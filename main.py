76fec1ede9f5f2ce57ebacb8d878cb43f6977de5
ebarnabo <edwinbarnabot@gmail.com>
2023-02-19 12:48:29 -0400

    Last Day

--- main.py
+++ main.py
@@ -1,1 +1,112 @@
-from pokemon import Pokemon
+import tkinter as tk
+from datetime import datetime
+from tkinter import simpledialog
+from tkinter import messagebox
+
+root = tk.Tk()
+root.withdraw()
+
+
+def quit(root):
+    root.destroy()
+
+
+
+# Demande le pseudo de l'utilisateur avec une boîte de dialogue
+user_name = simpledialog.askstring("Pseudo", "Entrez votre pseudo :")
+
+
+
+# Affiche la fenêtre principale
+root = tk.Toplevel()
+root.title("Super Chat 2.0")
+root.resizable(False, False)
+
+# Positionne la fenêtre au centre de l'écran
+window_width = root.winfo_screenwidth()
+window_height = root.winfo_screenheight()
+x = (window_width // 2) - (1200 // 2) # 1200 largeur
+y = (window_height // 2) - (300 // 2) # 300 hauteur
+root.geometry(f"1200x300+{x}+{y}")
+
+messages_frame = tk.Frame(root)
+scrollbar = tk.Scrollbar(messages_frame)
+scrollbar.pack(side=tk.RIGHT, fill=tk.Y)
+message_list = tk.Listbox(messages_frame, height=15, width=50, yscrollcommand=scrollbar.set)
+message_list.pack(side=tk.LEFT, fill=tk.BOTH)
+messages_frame.pack(side=tk.LEFT, fill=tk.BOTH, expand=True, padx=5, pady=5)
+
+# Affiche la liste des utilisateurs connectés au chat
+users_frame = tk.Frame(root)
+users_label = tk.Label(users_frame, text="Utilisateurs connectés")
+users_label.pack(side=tk.TOP)
+users_list = tk.Listbox(users_frame, height=15, width=20)
+users_list.pack(side=tk.LEFT, fill=tk.BOTH)
+users_frame.pack(side=tk.RIGHT, fill=tk.Y, padx=5, pady=5)
+entry_frame = tk.Frame(root, bd=2, padx=5, pady=5)
+email_icon = tk.Label(entry_frame, text="✉️", font=("Arial", 120))
+email_icon.pack(side=tk.TOP, padx=(0, 0), pady=(0, 20))
+entry_field = tk.Text(entry_frame, bd=0, font=("Arial", 12),insertbackground="#6876EC",selectbackground="#6876EC",borderwidth=0, height=5, width=50, fg="white")
+#entry_field.config(insertbackground="#6876EC", insertwidth=3, selectbackground="#6876EC", selectforeground="black")
+entry_field.pack(side=tk.LEFT, padx=5, pady=5, expand=True)
+entry_frame.pack(side=tk.BOTTOM, fill=tk.X, padx=5, pady=5)
+
+# Création du bouton pour les réactions emojis
+reactions_button = tk.Button(entry_frame, text="😀", bg="blue", font=("Arial", 12), bd=0, relief=tk.FLAT)
+reactions_button.pack(side=tk.TOP)
+
+# Liste des emojis de réaction
+reactions_list = [u'👍', u'👎', u'❤️', u'😂', u'😢', u'😠']
+
+# Fonction pour ajouter une réaction emoji à la zone de texte
+def add_reaction_emoji(emoji):
+    entry_field.insert(tk.END, emoji)
+
+# Fonction pour afficher la liste des réactions emojis
+def show_reactions_list(event=None):
+    # Création de la fenêtre popup
+    popup_window = tk.Toplevel(root)
+    popup_window.geometry(f"+{1080}+{350}") # positionne la fenêtre popup à droite et en dessous de la fenêtre principale
+    popup_window.resizable(False, False)
+    popup_window.overrideredirect(True) # enlève les bordures et les boutons de la fenêtre popup
+
+    # Création de la liste des réactions emojis
+    for emoji in reactions_list:
+        emoji_button = tk.Button(popup_window, text=emoji,font=("Arial", 12), bd=0, relief=tk.FLAT, command=lambda e=emoji: (add_reaction_emoji(e), popup_window.destroy()))
+        emoji_button.pack(side=tk.BOTTOM, padx=5, pady=5)
+
+# Ajout de l'événement d'affichage de la liste des réactions emojis
+reactions_button.bind("<Button-1>", show_reactions_list)
+
+
+def add_message(msg):
+    now = datetime.now()
+    current_time = now.strftime("%H:%M:%S")
+    message_list.insert(tk.END, f"[{current_time}] {msg}")
+
+def send_message(event=None):
+    msg = entry_field.get("1.0", "end-1c") # récupère le texte de la zone de texte, sans le caractère de fin de ligne
+    add_message("😂"+user_name+": "+ msg)
+    entry_field.delete("1.0", tk.END) # efface le contenu de la zone de texte
+
+
+def add_user(user):
+    users_list.insert(tk.END, user)
+
+
+def on_closing():
+    if messagebox.askokcancel("Quitter", "Êtes-vous sûr de vouloir quitter l'application ?"):
+        root.destroy()
+
+
+#tk.Button(root, text="Quit", command=lambda root=root:quit(root)).pack()
+
+root.protocol("WM_DELETE_WINDOW", on_closing)
+
+
+send_button = tk.Button(entry_frame, text="Envoyer", bg="blue", font=("Arial", 12), bd=0, relief=tk.FLAT, command=send_message)
+send_button.pack(side=tk.RIGHT)
+
+entry_field.bind("<Return>", send_message)
+
+root.mainloop()
--- server.py
+++ server.py
@@ -1,12 +1,114 @@
 import socket
+import select
 
-s = socket.socket(socket.AF_INET,socket.SOCK_STREAM)
-#print(socket.gethostname)
-s.bind((socket.gethostname(),1934))
-s.listen(5)
+HEADER_LENGTH = 10
+
+IP = "127.0.0.1"
+PORT = 1234
+
+# Créer un socket
+server_socket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+server_socket.setsockopt(socket.SOL_SOCKET, socket.SO_REUSEADDR, 1)
+#le serveur informe le système d'exploitation qu'il va utiliser l'adresse IP et le port donnés
+server_socket.bind((IP, PORT))
+
+# Le serveur écoute les nouvelles connexions
+server_socket.listen()
+
+# Liste des sockets pour select.select()
+sockets_list = [server_socket]
+
+# Liste des clients connectés
+clients = {}
+
+print(f'Serveur en attente de connexion {IP}:{PORT}...')
+
+# Handles message receiving
+def receive_message(client_socket):
+
+    try:
+
+        # Receive our "header" containing message length, it's size is defined and constant
+        message_header = client_socket.recv(HEADER_LENGTH)
+
+        # If we received no data, client gracefully closed a connection, for example using socket.close() or socket.shutdown(socket.SHUT_RDWR)
+        if not len(message_header):
+            return False
+
+        # Convert header to int value
+        message_length = int(message_header.decode('utf-8').strip())
+
+        # Return an object of message header and message data
+        return {'header': message_header, 'data': client_socket.recv(message_length)}
+
+    except:
+
+        # If we are here, client closed connection violently, for example by pressing ctrl+c on his script
+        # or just lost his connection
+        # socket.close() also invokes socket.shutdown(socket.SHUT_RDWR) what sends information about closing the socket (shutdown read/write)
+        # and that's also a cause when we receive an empty message
+        return False
 
 while True:
-    clientsocket, address = s.accept()
-    print(f"Connection from {address} has been established.")
-    clientsocket.send(bytes("Welcome","utf-8"))
-    msg = clientsocket.recv(1024)
+    read_sockets, _, exception_sockets = select.select(sockets_list, [], sockets_list)
+
+    for notified_socket in read_sockets:
+        if notified_socket == server_socket:
+
+            # Accepter la nouvelle connexion
+            client_socket, client_address = server_socket.accept()
+
+            # Le client doit envoyer son nom
+            user = receive_message(client_socket)
+
+            if user is False:
+                continue
+
+
+            sockets_list.append(client_socket)
+
+            clients[client_socket] = user
+
+            print('Connexion confirmée de {}:{}, pseudo: {}'.format(*client_address, user['data'].decode('utf-8')))
+
+        # Else existing socket is sending a message
+        else:
+
+            # Receive message
+            message = receive_message(notified_socket)
+
+            # If False, client disconnected, cleanup
+            if message is False:
+                print('Déconnexion de : {}'.format(clients[notified_socket]['data'].decode('utf-8')))
+
+                # Suppression de la liste 
+                sockets_list.remove(notified_socket)
+
+                # Suppression du client dans la liste
+                del clients[notified_socket]
+
+                continue
+
+            # Get user by notified socket, so we will know who sent the message
+            user = clients[notified_socket]
+
+            print(f'Nouveau message de {user["data"].decode("utf-8")}: {message["data"].decode("utf-8")}')
+
+            # Iterate over connected clients and broadcast message
+            for client_socket in clients:
+
+                # But don't sent it to sender
+                if client_socket != notified_socket:
+
+                    # Send user and message (both with their headers)
+                    # We are reusing here message header sent by sender, and saved username header send by user when he connected
+                    client_socket.send(user['header'] + user['data'] + message['header'] + message['data'])
+
+    # It's not really necessary to have this, but will handle some socket exceptions just in case
+    for notified_socket in exception_sockets:
+
+        # Remove from list for socket.socket()
+        sockets_list.remove(notified_socket)
+
+        # Remove from our list of users
+        del clients[notified_socket]
